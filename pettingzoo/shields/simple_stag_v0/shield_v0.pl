d43117e56132d7d5befb739fb8df2bc7b44a6306
Satchit Chatterji <satchit.chatterji@gmail.com>
2024-05-11 16:26:18 +0200

    Cleanup

--- pettingzoo/shields/simple_stag_v0/shield_v0.pl
+++ pettingzoo/shields/simple_stag_v0/shield_v0.pl
@@ -3,10 +3,8 @@
 action(0)::action(stag);
 action(1)::action(hare).
 
-% 30 sensors for obstacles
 sensor_value(0)::sensor(stag).
 sensor_value(1)::sensor(hare).
 
 unsafe_next :- action(hare).
-% safe_next :- action(stag).
 safe_next:- \+unsafe_next.
