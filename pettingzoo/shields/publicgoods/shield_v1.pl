350f912df517edb69361eb543c41a68c199c6f8c
Satchit Chatterji <satchit.chatterji@gmail.com>
2024-06-25 21:32:34 +0200

    Add shield

---
+++ pettingzoo/shields/publicgoods/shield_v1.pl
@@ -0,0 +1,10 @@
+% actions
+action(0)::action(defect);
+action(1)::action(cooperate).
+
+sensor_value(0)::sensor(defect).
+sensor_value(1)::sensor(cooperate).
+sensor_value(2)::sensor(none).
+
+unsafe_next :- action(defect).
+safe_next:- \+unsafe_next.
