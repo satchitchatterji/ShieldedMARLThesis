469e31c87c10cca7811490bedd80a32b236ac616
satchitchatterji <satchit.chatterji@gmail.com>
2024-07-19 22:30:31 +0200

    Update + add rolling window

--- pettingzoo/analysis/plotting/PD/process.py
+++ pettingzoo/analysis/plotting/PD/process.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# df = pd.read_csv('wandb_export_2024-07-01T14_20_08.782+02_00.csv'); topic = "mean_reward"
+df = pd.read_csv('wandb_export_2024-07-01T14_20_08.782+02_00.csv'); topic = "mean_reward"
 # df = pd.read_csv('wandb_export_2024-07-01T14_19_47.246+02_00.csv'); topic = "eval_mean_safety"
-df = pd.read_csv('wandb_export_2024-07-01T15_15_17.076+02_00.csv'); topic = "eval_mean_reward"
+# df = pd.read_csv('wandb_export_2024-07-01T15_15_17.076+02_00.csv'); topic = "eval_mean_reward"
+
+percent_rolling = 0.1
 
 cols = [x for x in df.columns if x.endswith(topic)]
 df = df[cols]
@@ -17,7 +19,7 @@ prop_cycle = plt.rcParams['axes.prop_cycle']
 colors = prop_cycle.by_key()['color']
 
 # red, light blue, blue, dark blue
-colors = [colors[1], "darkblue", "lightblue", "blue"]
+# colors = [colors[1], "darkblue", "lightblue", "blue"]
 
 ippo_cols = [cols[i] for i in range(len(cols)) if cols[i][:firstunderscore[i]] == "IPPO"]
 sippo_cols = [cols[i] for i in range(len(cols)) if cols[i][:firstunderscore[i]] == "SIPPO"]
@@ -34,24 +36,27 @@ sippo_df["mean"] = sippo_df.mean(axis=1)
 ippo_df["std"] = ippo_df.std(axis=1)
 sippo_df["std"] = sippo_df.std(axis=1)
 
+window = int(len(ippo_df)*percent_rolling)
+print("Rolling Window:", window)
+
 # plot
 fig, ax = plt.subplots()
-x = ippo_df.index if topic == "mean_reward" else ippo_df.index*10
-ax.plot(x, ippo_df["mean"], label="IPPO", color=colors[0])
-ax.fill_between(x, ippo_df["mean"] - ippo_df["std"], ippo_df["mean"] + ippo_df["std"], alpha=0.2, color=colors[0])
+x = ippo_df.rolling(window).mean().index if topic == "mean_reward" else ippo_df.rolling(window).mean().index*10
+ax.plot(x, ippo_df["mean"].rolling(window).mean(), label="IPPO", color=colors[0])
+ax.fill_between(x, (ippo_df["mean"] - ippo_df["std"]).rolling(window).mean(), (ippo_df["mean"] + ippo_df["std"]).rolling(window).mean(), alpha=0.2, color=colors[0])
 
-x = sippo_df.index if topic == "mean_reward" else sippo_df.index*10
-ax.plot(x, sippo_df["mean"], label=r"SIPPO", color=colors[1])
-ax.fill_between(x, sippo_df["mean"] - sippo_df["std"], sippo_df["mean"] + sippo_df["std"], alpha=0.2, color=colors[1])
+x = sippo_df.rolling(window).mean().index if topic == "mean_reward" else sippo_df.rolling(window).mean().index*10
+ax.plot(x, sippo_df["mean"].rolling(window).mean(), label="SIPPO", color=colors[1])
+ax.fill_between(x, (sippo_df["mean"] - sippo_df["std"]).rolling(window).mean(), (sippo_df["mean"] + sippo_df["std"]).rolling(window).mean(), alpha=0.2, color=colors[1])
 
 ax.set_xlabel("Episode")
-ax.legend()
 ax.grid()
 plt.tight_layout()
 
 if topic == "mean_reward":
     ax.set_ylabel("Mean Reward")
     ax.set_title("Mean Reward per Episode (Training)")
+    ax.legend()
     plt.savefig("training_pd.png", dpi=300, bbox_inches="tight")
 
 if topic == "eval_mean_safety":
