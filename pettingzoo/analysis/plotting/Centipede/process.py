0193f702686e2f3f0485d89b5e198b6c7377bf79
satchitchatterji <satchit.chatterji@gmail.com>
2024-07-20 16:37:52 +0200

    Plot Centipede

--- pettingzoo/analysis/plotting/Centipede/process.py
+++ pettingzoo/analysis/plotting/Centipede/process.py
@@ -1,87 +1,92 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
-df = pd.read_csv('wandb_export_2024-07-19T21_58_03.895+02_00.csv'); topic = "mean_reward"
-# df = pd.read_csv('wandb_export_2024-07-19T21_57_52.672+02_00.csv'); topic = "eval_mean_safety"
-# df = pd.read_csv('wandb_export_2024-07-19T21_57_38.021+02_00.csv'); topic = "eval_mean_reward"
+# topic = "total_reward_mean"
+# topic = "eval_mean_safety"
+# topic = "eval_total_reward_mean"
 
 percent_rolling = 0.1
 
+sippo_cols = ["SIPPO_2024-07-05_225742", "SIPPO_2024-07-05_225708", "SIPPO_2024-07-05_225444", "SIPPO_2024-07-05_225439", "SIPPO_2024-07-05_225412"]
+ippo_cols = ["IPPO_2024-07-05_224517", "IPPO_2024-07-05_224514", "IPPO_2024-07-05_224318"]
+
+explore_policy_softmax_SIQL = ["SIQL_2024-07-05_232934", "SIQL_2024-07-05_232850", "SIQL_2024-07-05_232837"]
+explore_policy_e_greedy_SIQL = ["SIQL_2024-07-05_231826", "SIQL_2024-07-05_231821", "SIQL_2024-07-05_231749"]
+explore_policy_softmax_IQL = ["IQL_2024-07-05_232241", "IQL_2024-07-05_232219", "IQL_2024-07-05_232142"]
+explore_policy_e_greedy_IQL = ["IQL_2024-07-05_231433", "IQL_2024-07-05_231427", "IQL_2024-07-05_231424"]
+
+group_ppo = [ippo_cols, sippo_cols]
+group_softmax = [explore_policy_softmax_IQL, explore_policy_softmax_SIQL]
+group_e_greedy = [explore_policy_e_greedy_IQL, explore_policy_e_greedy_SIQL]
+
+# labels = ["IPPO", "SIPPO"]; save_ext = "_cent_ppo.png"; plot_group = group_ppo
+# labels = ["IQL (softmax)", "SIQL (softmax)"]; save_ext = "_cent_softmax.png"; plot_group = group_softmax
+labels = ["IQL ($\epsilon$-greedy)", "SIQL ($\epsilon$-greedy)"]; save_ext = "_cent_e_greedy.png"; plot_group = group_e_greedy
+
+# get all csv files in directory
+files = os.listdir()
+files = [x for x in files if x.endswith(".csv")]
+
+# find file with topic in one of the columns
+correct_file = ""
+for file in files:
+    with open(file, "r") as f:
+        line = f.readline()
+        if "- "+topic in line:
+            correct_file = file
+            break
+
+print("File:", file)
+df = pd.read_csv(correct_file)
+print(df.columns)
+
 cols = [x for x in df.columns if x.endswith(topic)]
 df = df[cols]
-firstunderscore = [x.find("_") for x in cols] 
-firstspace = [x.find(" ") for x in cols] 
-
-algos = ["IPPO", "SIPPO"]
 
 # get default colour wheel for matplotlib
 prop_cycle = plt.rcParams['axes.prop_cycle']
 colors = prop_cycle.by_key()['color']
 
-# red, light blue, blue, dark blue
-# colors = [colors[1], "darkblue", "lightblue", "blue"]
+dfs = []
 
-# colors = ["darkred", "darkcyan", "purple"]
-# print(colors)
-# ippo_cols = [cols[i] for i in range(len(cols)) if cols[i][:firstunderscore[i]] == "IPPO"]
-# sippo_cols = [cols[i] for i in range(len(cols)) if cols[i][:firstunderscore[i]] == "SIPPO"]
+# ippo_df = df[[x+" - "+topic for x in ippo_cols]]
+# ippo_df["mean"] = ippo_df.mean(axis=1)
+# ippo_df["std"] = ippo_df.std(axis=1)
+for item in plot_group:
+    dfs.append(df[[x+" - "+topic for x in item]])
+    # take mean and std of each algo
+    dfs[-1]["mean"] = dfs[-1].mean(axis=1)
+    dfs[-1]["std"] = dfs[-1].std(axis=1)    
 
-ippo_runs = ["IPPO_2024-07-01_164539", "IPPO_2024-07-01_164532", "IPPO_2024-07-01_164520", "IPPO_2024-07-01_164514", "IPPO_2024-07-01_164509"]
-sippo_runs_v1 = ["SIPPO_2024-07-01_200634", "SIPPO_2024-07-01_200623", "SIPPO_2024-07-01_200617", "SIPPO_2024-07-01_200610", "SIPPO_2024-07-01_195959"]
-sippo_runs_v0 = ["SIPPO_2024-07-01_164115", "SIPPO_2024-07-01_164110", "SIPPO_2024-07-01_163655", "SIPPO_2024-07-01_163649", "SIPPO_2024-07-01_163643"]
-
-
-ippo_df = df[[x+" - "+topic for x in ippo_runs]]
-sippo_df_v0 = df[[x+" - "+topic for x in sippo_runs_v0]]
-sippo_df_v1 = df[[x+" - "+topic for x in sippo_runs_v1]]
-
-# take mean and std of each algo
-ippo_df["mean"] = ippo_df.mean(axis=1)
-sippo_df_v0["mean"] = sippo_df_v0.mean(axis=1)
-sippo_df_v1["mean"] = sippo_df_v1.mean(axis=1)
-
-ippo_df["std"] = ippo_df.std(axis=1)
-sippo_df_v0["std"] = sippo_df_v0.std(axis=1)
-sippo_df_v1["std"] = sippo_df_v1.std(axis=1)
-
-print(len(ippo_df))
-window = int(len(ippo_df)*percent_rolling)
-print("Rolling Window:", window)
-
+window = int(len(dfs[0])*percent_rolling)
 # plot
 fig, ax = plt.subplots()
-x = ippo_df.rolling(window).mean().index if topic == "mean_reward" else ippo_df.rolling(window).mean().index*10
-ax.plot(x, ippo_df["mean"].rolling(window).mean(), label="IPPO", color=colors[0])
-ax.fill_between(x, (ippo_df["mean"] - ippo_df["std"]).rolling(window).mean(), (ippo_df["mean"] + ippo_df["std"]).rolling(window).mean(), alpha=0.2, color=colors[0])
 
-x = sippo_df_v0.rolling(window).mean().index if topic == "mean_reward" else sippo_df_v0.rolling(window).mean().index*10
-ax.plot(x, sippo_df_v0["mean"].rolling(window).mean(), label=r"SIPPO (pure strategy)", color=colors[1])
-ax.fill_between(x, (sippo_df_v0["mean"] - sippo_df_v0["std"]).rolling(window).mean(), (sippo_df_v0["mean"] + sippo_df_v0["std"]).rolling(window).mean(), alpha=0.2, color=colors[1])
-
-x = sippo_df_v1.rolling(window).mean().index if topic == "mean_reward" else sippo_df_v1.rolling(window).mean().index*10
-ax.plot(x, sippo_df_v1["mean"].rolling(window).mean(), label=r"SIPPO (mixed strategy)", color=colors[2])
-ax.fill_between(x, (sippo_df_v1["mean"] - sippo_df_v1["std"]).rolling(window).mean(), (sippo_df_v1["mean"] + sippo_df_v1["std"]).rolling(window).mean(), alpha=0.2, color=colors[2])
+for i, df in enumerate(dfs):
+    x = df.rolling(window).mean().index if topic == "total_reward_mean" else df.rolling(window).mean().index*10
+    ax.plot(x, df["mean"].rolling(window).mean(), label=labels[i], color=colors[i])
+    ax.fill_between(x, (df["mean"] - df["std"]).rolling(window).mean(), (df["mean"] + df["std"]).rolling(window).mean(), alpha=0.2, color=colors[i])
 
 ax.set_xlabel("Episode")
-
 ax.grid()
 plt.tight_layout()
 
-if topic == "mean_reward":
-    ax.set_ylabel("Mean Reward")
+if topic == "total_reward_mean":
+    ax.set_ylabel("Reward")
     ax.set_title("Mean Reward per Episode (Training)")
-    plt.legend()
-    plt.savefig("training_centipede.png", dpi=300, bbox_inches="tight")
+    ax.legend(loc="center left")
+    plt.savefig(f"training{save_ext}", dpi=300, bbox_inches="tight")
 
 if topic == "eval_mean_safety":
-    ax.set_ylabel("Mean Action==Stag")
-    ax.set_title("Mean Stag (Safety) per Episode")
-    plt.savefig("safety_centipede.png", dpi=300, bbox_inches="tight")
+    ax.set_ylabel("Mean Action==Continue")
+    ax.set_title("Mean Continue (Safety) per Episode")
+    plt.savefig(f"safety{save_ext}", dpi=300, bbox_inches="tight")
 
-if topic == "eval_mean_reward":
-    ax.set_ylabel("Mean Reward")
+if topic == "eval_total_reward_mean":
+    ax.set_ylabel("Reward")
     ax.set_title("Mean Reward per Episode (Evaluation)")
-    plt.savefig("eval_centipede.png", dpi=300, bbox_inches="tight")
+    plt.savefig(f"eval{save_ext}", dpi=300, bbox_inches="tight")
 
 
 plt.show()
