3d9d8d06110e13190e9885dfe0c4ba870af91689
satchitchatterji <satchit.chatterji@gmail.com>
2024-07-22 14:38:04 +0200

    MSH Plots

---
+++ pettingzoo/analysis/plotting/Markov SH/export_api.py
@@ -0,0 +1,50 @@
+import pandas as pd 
+import wandb
+import tqdm
+api = wandb.Api()
+
+# Project is specified by <entity/project-name>
+project = "satchat/nt_markov_stag_hunt"
+keys = ["stag_training", "stag_pen_training", "plant_training"]
+
+runs = api.runs(project)
+
+summary_list, config_list, name_list = [], [], []
+for run in runs: 
+    # .summary contains the output keys/values for metrics like accuracy.
+    #  We call ._json_dict to omit large files 
+    summary_list.append(run.summary._json_dict)
+
+    # .config contains the hyperparameters.
+    #  We remove special values that start with _.
+    config_list.append(
+        {k: v for k,v in run.config.items()
+          if not k.startswith('_')})
+
+    # .name is the human-readable name of the run.
+    name_list.append(run.name)
+
+runs_df = pd.DataFrame({
+    "config": config_list,
+    "name": name_list
+    })
+
+
+runs_df.to_csv("project.csv")
+
+dfs = {key: list() for key in keys}
+for run in tqdm.tqdm(runs, desc="Downloading runs"):
+    name = run.name
+    for key in keys:
+        try:
+            col_name = f"{name} - {key}"
+            hist_df = run.history(keys=[key], pandas=True)
+            hist_df = hist_df.rename(columns={key: f"{name} - {key}"})
+            dfs[key].append(hist_df[col_name])
+        except KeyError as e:
+            print(f"Key Error: {e}")
+
+for key in keys:
+    df = pd.concat(dfs[key], axis=1)
+    df.to_csv(f"{key}.csv")
+
