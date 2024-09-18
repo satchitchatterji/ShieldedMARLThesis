495bd9656b756cd5981dfeb88e5c20cfef9c4ddb
satchitchatterji <satchit.chatterji@gmail.com>
2024-09-18 14:59:40 +0200

    Re-added system id in wandb project initialization

--- pettingzoo/simple_spread.py
+++ pettingzoo/simple_spread.py
@@ -20,8 +20,8 @@ from run_episode import run_episode, eval_episode
 np.random.seed(config.seed)
 torch.manual_seed(config.seed)
 
-# system = os.name
-system = "nt"
+system = os.name
+
 # cur_time = time.time()
 now = datetime.datetime.now()
 cur_time = now.strftime("%Y-%m-%d_%H%M%S")
